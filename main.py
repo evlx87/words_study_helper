6f358b9fd24d31484999d30ad0a6e153b077e9ea
evlx87 <evlx@mail.ru>
2023-07-12 23:21:11 +0300

    changed choose_random_word() to choose_word(), added question_answer() and result_output()

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ answers = {}
 
 
 def choose_dictionary(difficulty):
-    """Функция для выбора сложности"""
+    """Функция для выбора сложности и выбора словаря"""
     difficulty_mapping = {
         "1": WORDS_EASY, "легкий": WORDS_EASY, "easy": WORDS_EASY,
         "2": WORDS_MEDIUM, "средний": WORDS_MEDIUM, "medium": WORDS_MEDIUM,
@@ -47,14 +47,47 @@ def choose_dictionary(difficulty):
     return difficulty_mapping.get(difficulty)
 
 
-def choose_random_word(dictionary):
-    """Получаем случайное слово, перевод, длину и первую букву"""
-    word, word_translate = random.choice(list(dictionary.items()))
+def choose_word(iterator):
+    """Получаем слово из словаря, его перевод, длину и первую букву"""
+    word, word_translate = next(iterator)
     word_length = len(word)
-    word_first_letter = word[0]
-    return word, word_translate, word_length, word_first_letter
+    word_first_letter = word_translate[0]
+    question_string = f"{word.capitalize()}, {word_length} букв, начинается на {word_first_letter}..."
+    return question_string, word_translate, word
 
 
+def question_answer(dictionary):
+    """Вывод вопроса и получения ответа"""
+    iterator = iter(dictionary.items())
+    for _ in dictionary:
+        question_string, word_translate, word = choose_word(iterator)
+        print(question_string)
+        user_answer = input("Введите ваш ответ: ").lower()
+        if user_answer == word_translate:
+            print(f"Верно. {word.capitalize()} - это {word_translate}")
+            answers[word] = True
+        else:
+            print(f"Неверно. {word.capitalize()} - это {word_translate}")
+            answers[word] = False
+
+
+def result_output(answers_dictionary):
+    """Вывод результата"""
+    answers_true = [word for word, value in answers_dictionary.items() if value]
+    answers_false = [word for word, value in answers_dictionary.items() if not value]
+    # Выдаем пользователю список верных/неверных ответов
+    print("Правильно отвечены слова:")
+    for word in answers_true:
+        print(word)
+    print("\nНеправильно отвечены слова:")
+    for word in answers_false:
+        print(word)
+    # Высчитываем ранг пользователя
+    user_result = len(answers_true)
+    print(f"\nВаш ранг:\n{LEVELS[user_result]}")
+
+
+# Запрашиваем у пользователя уровень сложности и выбираем нужный словарь
 print('''Выберите уровень сложности:
 1 - Легкий
 2 - Средний
@@ -62,6 +95,7 @@ print('''Выберите уровень сложности:
 user_select = input("Введите ваш выбор: ").lower()
 
 words = choose_dictionary(user_select)
-print(type(words))
 
-print(choose_random_word(words))
+question_answer(words)
+
+result_output(answers)
